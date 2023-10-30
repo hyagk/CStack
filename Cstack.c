21277b6ad183d5fb50687ce960ebc17a48a31030
hyagk <145639863+hyagk@users.noreply.github.com>
2023-10-30 14:28:25 +0900

    Add files via upload

---
+++ Cstack.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+#define MAX_SIZE 3
+int stack[MAX_SIZE];
+int top = -1; 
+
+
+void push(int value) {
+    if (top < MAX_SIZE - 1) {
+        stack[++top] = value;
+    }
+    else {
+        printf("Stack is full. Cannot push %d\n", value);
+    }
+}
+
+int pop() {
+    if (top >= 0) {
+        return stack[top--];
+    }
+    else {
+        printf("Stack is empty. Cannot pop.\n");
+        return -1; 
+    }
+}
+
+int main() {
+    push(1);
+    push(2);
+    push(3);
+
+    int poppedValue1 = pop();
+    int poppedValue2 = pop();
+    int poppedValue3 = pop();
+
+    printf("Popped values: %d, %d, %d\n", poppedValue1, poppedValue2, poppedValue3);
+
+    return 0;
+}
